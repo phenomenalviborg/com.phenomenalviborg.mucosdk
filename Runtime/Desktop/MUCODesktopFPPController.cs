e650ca5a10cf3487b9a8155ff11ecb062a4091b9
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-19 11:01:35 +0100

    MUCOSDK-15

--- Runtime/Desktop/MUCODesktopFPPController.cs
+++ Runtime/Desktop/MUCODesktopFPPController.cs
@@ -7,10 +7,11 @@ namespace PhenomenalViborg.MUCOSDK
     [RequireComponent(typeof(CharacterController))]
     public class MUCODesktopFPPController : MonoBehaviour
     {
-        // public AnimationCurve Bobcurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1f, 0f), new Keyframe(1.5f, -1f), new Keyframe(2f, 0f)); // sin curve for head bob
-        [SerializeField] private float m_WalkSpeed = 5.0f;
         [SerializeField] private Camera m_Camera;
+        [SerializeField] private float m_WalkSpeed = 5.0f;
         [SerializeField] private float m_Sensitivity = 2.0f; // This should properly be moved to some sort of player settings system.
+
+        // TODO: Head-bob
 
         private CharacterController m_CharacterController;
 
@@ -28,7 +29,7 @@ namespace PhenomenalViborg.MUCOSDK
             transform.rotation *= Quaternion.Euler(0.0f, inputY, 0.0f);
             m_Camera.transform.rotation *= Quaternion.Euler(-inputX, 0.0f, 0.0f);
 
-            // Update translation
+            // Update position
             Vector3 moveDirection = m_Camera.transform.forward * Input.GetAxis("Vertical") + m_Camera.transform.right * Input.GetAxis("Horizontal");
             moveDirection.y = 0.0f;
             moveDirection = moveDirection.normalized * m_WalkSpeed;
